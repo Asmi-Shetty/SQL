423687f0cfca10cc9f9c67c37968cd930e487699
Asmi-Shetty <askmeshetty2003@gmail.com>
2026-07-14 18:03:32 +0530

    commit

---
+++ Averages.sql
@@ -0,0 +1,11 @@
+SELECT AVG(POPULATION) FROM CITY
+WHERE DISTRICT = 'CALIFORNIA'
+
+
+/* AVG is the keyword NOT AVERAGE
+
+
+/*
+Query the average population of all cities in 
+CITY where District is California.
+*/
