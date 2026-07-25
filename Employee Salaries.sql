d5da2881f635449e252cbd07809bc5bdcec3e9ff
Asmi-Shetty <askmeshetty2003@gmail.com>
2026-07-25 22:55:36 +0530

    commit

--- Employee Salaries.sql
+++ Employee Salaries.sql
@@ -8,6 +8,5 @@ Write a query that prints a list of employee names
 (that is - the name attribute) for employees in Employee
  having a salary greater than 2000 per month who 
  have been employees for less than 10 months. 
- Sort your result by ascending employee_id.
-
+ Sort your result by ascending employee_id
 */
