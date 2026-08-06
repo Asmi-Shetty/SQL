ce4a0ec265ead98471f7261c0e6d2cc418b4aefc
Asmi-Shetty <askmeshetty2003@gmail.com>
2026-08-06 20:23:42 +0530

    commit

--- we.sql
+++ we.sql
@@ -7,7 +7,8 @@ ORDER BY RIGHT(Name, 3), ID
 Query the Name of any student in STUDENTS who scored higher than 75  Marks. 
 Order your output by the last three characters of 
 
-each name. If two or more students both have names
+
+each name, If two or more students both have names
  ending in the same last three characters
   (that is Bobby, Robby, etc.) secondary sort them by 
   ascending ID)
