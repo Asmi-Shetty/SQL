380623c43fcff8bdbb44ae8629f6be8e53d02547
Asmi-Shetty <askmeshetty2003@gmail.com>
2026-08-24 23:44:04 +0530

    changed

--- Weather Observation Station 7.sql
+++ Weather Observation Station 7.sql
@@ -4,7 +4,7 @@ WHERE RIGHT(CITY, 1) IN ('a','e','i','o','u')
 
 /*
 
-Query the list of CITY names ending with vowels (a, e, i, o, u) 
+Query the list of CITY names ending with the vowels (a, e, i, o, u) 
 from STATION. Your result cannot contain duplicates.
 
 */
